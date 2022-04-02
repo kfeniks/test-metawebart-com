20aa3f753715e45fcde0265586d530b1ab3c0079
Alex Du <keccgroup@gmail.com>
2022-04-02 23:11:09 +0300

    Task 2: edit native code, remove useless function

--- src/index-native-code.php
+++ src/index-native-code.php
@@ -11,36 +11,6 @@ declare(strict_types=1);
  * @param string $pattern
  * @return string
  */
-function validateWithoutPattern(string $value, string $pattern): string
-{
-    $patternsList = str_split($pattern);
-    var_dump($patternsList);die;
-    $brackets = preg_replace("/[^$pattern\s]/", '', $value);
-    $pattern = substr($pattern, 0, 1);
-    $stack = [];
-    
-    var_dump($brackets);die;
-    
-    for ($i = 0, $all = strlen($brackets); $i < $all; $i++) {
-        $element = $brackets[$i];
-        if ($element == $pattern) {
-            array_unshift($stack, $element);
-        } elseif (!empty($stack)) {
-//            $key = array_search('green', $array);
-//            array_shift($stack);
-        }
-    }
-    
-    var_dump($stack);die;
-    
-    return count($stack) === 0 ? 'Correct' : 'Wrong';
-}
-
-/**
- * @param string $value
- * @param string $pattern
- * @return string
- */
 function validate(string $value, string $pattern): string
 {
     $chars = 'a-zA-Z\s+';
