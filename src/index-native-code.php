ca2e65cb58e67008e5bcb0a95cedafda32f81a77
Alex Du <keccgroup@gmail.com>
2022-04-02 23:09:28 +0300

    Task 2: add logic and functions

--- src/index-native-code.php
+++ src/index-native-code.php
@@ -11,23 +11,106 @@ declare(strict_types=1);
  * @param string $pattern
  * @return string
  */
-function validate(string $value, string $pattern): string
+function validateWithoutPattern(string $value, string $pattern): string
 {
-    $brackets = preg_replace("~[^$pattern]~", '', $value);
+    $patternsList = str_split($pattern);
+    var_dump($patternsList);die;
+    $brackets = preg_replace("/[^$pattern\s]/", '', $value);
     $pattern = substr($pattern, 0, 1);
     $stack = [];
+    
+    var_dump($brackets);die;
     
     for ($i = 0, $all = strlen($brackets); $i < $all; $i++) {
         $element = $brackets[$i];
         if ($element == $pattern) {
             array_unshift($stack, $element);
         } elseif (!empty($stack)) {
-            array_shift($stack);
+//            $key = array_search('green', $array);
+//            array_shift($stack);
+        }
+    }
+    
+    var_dump($stack);die;
+    
+    return count($stack) === 0 ? 'Correct' : 'Wrong';
+}
+
+/**
+ * @param string $value
+ * @param string $pattern
+ * @return string
+ */
+function validate(string $value, string $pattern): string
+{
+    $chars = 'a-zA-Z\s+';
+    $brackets = preg_replace('/['.$chars.']/', '', $value);
+    $availablePatterns = str_split($pattern);
+    $stack = [];
+    
+    for ($i = 0, $all = strlen($brackets); $i < $all; $i++) {
+        $element = $brackets[$i];
+        if (in_array($element, $availablePatterns) ) {
+            if (in_array($element, $stack)) {
+                $key = array_search($element, $stack);
+                unset($stack[$key]);
+            } else {
+                $revertCharacter = revertCharacter($element);
+                if(!$revertCharacter) {
+                    continue;
+                }
+                
+                if (in_array($element, closingSigns())) {
+                    return 'Wrong';
+                }
+                
+                array_unshift($stack, $revertCharacter);
+            }
         }
     }
     
     return count($stack) === 0 ? 'Correct' : 'Wrong';
 }
 
-$str = 'Строка с ( вложенными (скобками) для () примера)';
-echo 'Result is: ' . validate($str, '()') . PHP_EOL;
+/**
+ * @param string $value
+ * @return false|string
+ */
+function revertCharacter(string $value)
+{
+    switch($value) {
+        case '(':
+            $revertElement = ')';
+            break;
+        case '{':
+            $revertElement = '}';
+            break;
+        case '[':
+            $revertElement = ']';
+            break;
+        case ')':
+            $revertElement = '(';
+            break;
+        case '}':
+            $revertElement = '{';
+            break;
+        case ']':
+            $revertElement = '[';
+            break;
+        default:
+            $revertElement = false;
+    }
+    
+    return $revertElement;
+}
+
+/**
+ * @return string[]
+ */
+function closingSigns(): array
+{
+    return [')', '}', ']'];
+}
+
+$strCorrect = 'String with ( nested (brackets) for () example [])';
+echo 'Result is: ' . validate($strCorrect, '[({})]') . PHP_EOL;
